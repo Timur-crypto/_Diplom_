6ca09db8d82bb2022a72b700f2f50ac940ded6f3
Timur-crypto <tuxvatullin.tima@inbox.ru>
2025-02-09 04:22:05 +0500

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Тимур 24-я когорта.Финальный проект.Инжинер по тестированию плюс
+URL_SERVICE = "https://e6621b12-9562-4cf2-9724-98238cd7ac74.serverhub.praktikum-services.ru/"
+CREATE_ORDERS = "/api/v1/orders"
+FETCH_ORDER_TRACK = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,12 @@
+# Тимур 24-я когорта.Финальный проект.Инжинер по тестированию плюс
+order_body = {
+        "firstName": "Timur",
+        "lastName": "Shomil",
+        "address": "Salam",
+        "metroStation": 2,
+        "phone": "+89345654566",
+        "rentTime": 3,
+        "deliveryDate": "2025-02-11",
+        "comment": "Welcome",
+        "color": ["BLACK"]
+    }
