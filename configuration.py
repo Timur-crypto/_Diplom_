e87f53a8a6e0c2654cfca461ad4a92cef4e243b2
Timur-crypto <tuxvatullin.tima@inbox.ru>
2025-02-09 18:20:55 +0500

    Add files via upload
    В константе URL_SERVICE удалил слеш в конце ссылки

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-# Тимур 24-я когорта.Финальный проект.Инжинер по тестированию плюс
-URL_SERVICE = "https://e6621b12-9562-4cf2-9724-98238cd7ac74.serverhub.praktikum-services.ru/"
+# Тимур 24-я когорта.Финальный проект.Инженер по тестированию плюс
+URL_SERVICE = "https://0a9cfc34-91a3-4f60-a0ed-cac149f7bacf.serverhub.praktikum-services.ru"
 CREATE_ORDERS = "/api/v1/orders"
 FETCH_ORDER_TRACK = "/api/v1/orders/track"
